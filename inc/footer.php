67503459a1085d1b09bebef2b82d76bae591ac22
rgonzalezjuvinao <145308282+rgonzalezjuvinao@users.noreply.github.com>
2024-05-26 13:19:28 -0500

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,18 @@
+<footer>
+    <h3 class="text-center">Siguenos en nuestras redes sociales</h3><br>
+    <ul class="list-unstyled text-center">
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Facebook">
+            <img src="assets/icons/social-facebook.png" alt="facebook-icon">
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Instagram">
+            <img src="assets/icons/social-instagram.png" alt="instagram-icon">
+        </a>
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="Linkedin">
+            <img src="assets/icons/social-linkedin.png" alt="linkedin-icon">
+        <a href="#" class="social-icon all-elements-tooltip" data-toggle="tooltip" data-placement="bottom" title="WhatsApp">
+            <img src="assets/icons/Whatsapp.png" alt="Red social WhatsApp-icon">
+        </a>
+    </ul>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo">Tecno Store Colombia &copy; 2024 - Desing: Yudy Aroca & Ricardo Gonzalez</h5>
+</footer>
